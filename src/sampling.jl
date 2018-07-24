3d31518c6e89a85df6189482596afe997410d1bd
Rafael Fourquet <fourquet.rafael@gmail.com>
2018-07-24 15:16:56 +0200

    Sampler: use type instead of instance for the RNG argument
    Cf. https://github.com/JuliaLang/julia/pull/28010

--- src/sampling.jl
+++ src/sampling.jl
@@ -3,16 +3,16 @@
 
 ## Uniform
 
-Sampler(rng::AbstractRNG, d::Union{UniformWrap,UniformType}, n::Repetition) =
-    Sampler(rng, d[], n)
+Sampler(RNG::Type{<:AbstractRNG}, d::Union{UniformWrap,UniformType}, n::Repetition) =
+    Sampler(RNG, d[], n)
 
 
 ## floats
 
 ### override def from Random
 
-Sampler(rng::AbstractRNG, ::Type{T}, n::Repetition) where {T<:AbstractFloat} =
-    Sampler(rng, CloseOpen01(T), n)
+Sampler(RNG::Type{<:AbstractRNG}, ::Type{T}, n::Repetition) where {T<:AbstractFloat} =
+    Sampler(RNG, CloseOpen01(T), n)
 
 ### fall-back on Random definitions
 rand(r::AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {T} =
@@ -23,8 +23,8 @@ rand(r::AbstractRNG, ::SamplerTrivial{CloseOpen12{T}}) where {T} =
 
 ### CloseOpenAB
 
-Sampler(rng::AbstractRNG, d::CloseOpenAB{T}, n::Repetition) where {T} =
-    SamplerTag{CloseOpenAB{T}}((a=d.a, d=d.b - d.a, sp=Sampler(rng, CloseOpen01{T}(), n)))
+Sampler(RNG::Type{<:AbstractRNG}, d::CloseOpenAB{T}, n::Repetition) where {T} =
+    SamplerTag{CloseOpenAB{T}}((a=d.a, d=d.b - d.a, sp=Sampler(RNG, CloseOpen01{T}(), n)))
 
 rand(rng::AbstractRNG, sp::SamplerTag{CloseOpenAB{T}}) where {T} =
     sp.data.a + sp.data.d  * rand(rng, sp.data.sp)
@@ -32,9 +32,9 @@ rand(rng::AbstractRNG, sp::SamplerTag{CloseOpenAB{T}}) where {T} =
 
 ## sampler for pairs and complex numbers
 
-function Sampler(rng::AbstractRNG, u::Combine2{T}, n::Repetition) where T <: Union{Pair,Complex}
-    sp1 = Sampler(rng, u.x, n)
-    sp2 = u.x == u.y ? sp1 : Sampler(rng, u.y, n)
+function Sampler(RNG::Type{<:AbstractRNG}, u::Combine2{T}, n::Repetition) where T <: Union{Pair,Complex}
+    sp1 = Sampler(RNG, u.x, n)
+    sp2 = u.x == u.y ? sp1 : Sampler(RNG, u.y, n)
     SamplerTag{Cont{T}}((sp1, sp2))
 end
 
@@ -44,11 +44,11 @@ rand(rng::AbstractRNG, sp::SamplerTag{Cont{T}}) where {T<:Union{Pair,Complex}} =
 
 ### additional methods for complex numbers
 
-Sampler(rng::AbstractRNG, u::Combine1{Complex}, n::Repetition) =
-    Sampler(rng, Combine(Complex, u.x, u.x), n)
+Sampler(RNG::Type{<:AbstractRNG}, u::Combine1{Complex}, n::Repetition) =
+    Sampler(RNG, Combine(Complex, u.x, u.x), n)
 
-Sampler(rng::AbstractRNG, ::Type{Complex{T}}, n::Repetition) where {T<:Real} =
-    Sampler(rng, Combine(Complex, T, T), n)
+Sampler(RNG::Type{<:AbstractRNG}, ::Type{Complex{T}}, n::Repetition) where {T<:Real} =
+    Sampler(RNG, Combine(Complex, T, T), n)
 
 
 ## Normal & Exponential
@@ -56,8 +56,8 @@ Sampler(rng::AbstractRNG, ::Type{Complex{T}}, n::Repetition) where {T<:Real} =
 rand(rng::AbstractRNG, ::SamplerTrivial{Normal01{T}}) where {T<:Union{AbstractFloat,Complex{<:AbstractFloat}}} =
     randn(rng, T)
 
-Sampler(rng::AbstractRNG, d::Normalμσ{T}, n::Repetition) where {T} =
-    SamplerSimple(d, Sampler(rng, Normal(T), n))
+Sampler(RNG::Type{<:AbstractRNG}, d::Normalμσ{T}, n::Repetition) where {T} =
+    SamplerSimple(d, Sampler(RNG, Normal(T), n))
 
 rand(rng::AbstractRNG, sp::SamplerSimple{Normalμσ{T},<:Sampler}) where {T} =
     sp[].μ + sp[].σ  * rand(rng, sp.data)
@@ -65,8 +65,8 @@ rand(rng::AbstractRNG, sp::SamplerSimple{Normalμσ{T},<:Sampler}) where {T} =
 rand(rng::AbstractRNG, ::SamplerTrivial{Exponential1{T}}) where {T<:AbstractFloat} =
     randexp(rng, T)
 
-Sampler(rng::AbstractRNG, d::Exponentialθ{T}, n::Repetition) where {T} =
-    SamplerSimple(d, Sampler(rng, Exponential(T), n))
+Sampler(RNG::Type{<:AbstractRNG}, d::Exponentialθ{T}, n::Repetition) where {T} =
+    SamplerSimple(d, Sampler(RNG, Exponential(T), n))
 
 rand(rng::AbstractRNG, sp::SamplerSimple{Exponentialθ{T},<:Sampler}) where {T} =
     sp[].θ * rand(rng, sp.data)
